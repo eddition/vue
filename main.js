dbf7128b8a7093a4f8b71886bf89c6f302ce7de0
Eddie Shin <eshin0910@gmail.com>
2016-08-30 23:09:37 -0400

    first

---
+++ main.js
@@ -0,0 +1,423 @@
+var mockData = {
+movies: [
+{
+id: "771403560",
+title: "Don't Breathe",
+year: 2016,
+mpaa_rating: "R",
+runtime: 88,
+critics_consensus: "",
+release_dates: {
+theater: "2016-08-26"
+},
+ratings: {
+critics_rating: "Certified Fresh",
+critics_score: 86,
+audience_rating: "Upright",
+audience_score: 85
+},
+synopsis: "A trio of reckless thieves breaks into the house of a wealthy blind man, thinking they'll get away with the perfect heist. They're wrong.",
+posters: {
+thumbnail: "https://resizing.flixster.com/5NmEDWvAfkEWyaqzce1VGalZ6Us=/52x81/v1.bTsxMTcwODA5MjtqOzE3MDkyOzIwNDg7NTIzOzgxNg",
+profile: "https://resizing.flixster.com/5NmEDWvAfkEWyaqzce1VGalZ6Us=/52x81/v1.bTsxMTcwODA5MjtqOzE3MDkyOzIwNDg7NTIzOzgxNg",
+detailed: "https://resizing.flixster.com/5NmEDWvAfkEWyaqzce1VGalZ6Us=/52x81/v1.bTsxMTcwODA5MjtqOzE3MDkyOzIwNDg7NTIzOzgxNg",
+original: "https://resizing.flixster.com/5NmEDWvAfkEWyaqzce1VGalZ6Us=/52x81/v1.bTsxMTcwODA5MjtqOzE3MDkyOzIwNDg7NTIzOzgxNg"
+},
+abridged_cast: [
+{
+name: "Jane Levy",
+id: "771386601",
+characters: [
+"Rocky"
+]
+},
+{
+name: "Dylan Minnette",
+id: "770808601",
+characters: [
+"Alex"
+]
+},
+{
+name: "Daniel Zovatto",
+id: "771449326",
+characters: [
+"Money"
+]
+},
+{
+name: "Stephen Lang",
+id: "162662195",
+characters: [
+"The Blind Man"
+]
+},
+{
+name: "Emma Bercovici",
+id: "771834426",
+characters: [
+"Diddy"
+]
+}
+],
+links: {
+self: "http://api.rottentomatoes.com/api/public/v1.0/movies/771403560.json",
+alternate: "http://www.rottentomatoes.com/m/dont_breathe_2016/",
+cast: "http://api.rottentomatoes.com/api/public/v1.0/movies/771403560/cast.json",
+reviews: "http://api.rottentomatoes.com/api/public/v1.0/movies/771403560/reviews.json",
+similar: "http://api.rottentomatoes.com/api/public/v1.0/movies/771403560/similar.json"
+}
+},
+{
+id: "771400429",
+title: "Suicide Squad",
+year: 2016,
+mpaa_rating: "PG-13",
+runtime: 123,
+critics_consensus: "",
+release_dates: {
+theater: "2016-08-05"
+},
+ratings: {
+critics_rating: "Rotten",
+critics_score: 26,
+audience_rating: "Upright",
+audience_score: 68
+},
+synopsis: "From director David Ayer ('Fury,' 'End of Watch') comes 'Suicide Squad,' starring Oscar nominee Will Smith , Oscar winner Jared Leto , Margot Robbie, Joel Kinnaman and Oscar nominee Viola Davis . It feels good to be bad... Assemble a team of the world's most dangerous, incarcerated Super Villains, provide them with the most powerful arsenal at the government's disposal, and send them off on a mission to defeat an enigmatic, insuperable entity. U.S. intelligence officer Amanda Waller has determined only a secretly convened group of disparate, despicable individuals with next to nothing to lose will do. However, once they realize they weren't picked to succeed but chosen for their patent culpability when they inevitably fail, will the Suicide Squad resolve to die trying, or decide it's every man for himself?",
+posters: {
+thumbnail: "https://resizing.flixster.com/TJ4b-9w0IulKxCZHSQNT4TnVZmY=/54x80/v1.bTsxMjA2MTc4MTtqOzE3MDg1OzIwNDg7NjQ4Ozk2MA",
+profile: "https://resizing.flixster.com/TJ4b-9w0IulKxCZHSQNT4TnVZmY=/54x80/v1.bTsxMjA2MTc4MTtqOzE3MDg1OzIwNDg7NjQ4Ozk2MA",
+detailed: "https://resizing.flixster.com/TJ4b-9w0IulKxCZHSQNT4TnVZmY=/54x80/v1.bTsxMjA2MTc4MTtqOzE3MDg1OzIwNDg7NjQ4Ozk2MA",
+original: "https://resizing.flixster.com/TJ4b-9w0IulKxCZHSQNT4TnVZmY=/54x80/v1.bTsxMjA2MTc4MTtqOzE3MDg1OzIwNDg7NjQ4Ozk2MA"
+},
+abridged_cast: [
+{
+name: "Will Smith",
+id: "162659361",
+characters: [
+"Floyd Lawton/Deadshot"
+]
+},
+{
+name: "Jared Leto",
+id: "162653925",
+characters: [
+"The Joker"
+]
+},
+{
+name: "Margot Robbie",
+id: "770873485",
+characters: [
+"Dr. Harleen F. Quinzel/Harley Quinn"
+]
+},
+{
+name: "Joel Kinnaman",
+id: "653490685",
+characters: [
+"Rick Flagg"
+]
+},
+{
+name: "Viola Davis",
+id: "162655119",
+characters: [
+"Amanda Waller"
+]
+}
+],
+links: {
+self: "http://api.rottentomatoes.com/api/public/v1.0/movies/771400429.json",
+alternate: "http://www.rottentomatoes.com/m/suicide_squad_2016/",
+cast: "http://api.rottentomatoes.com/api/public/v1.0/movies/771400429/cast.json",
+reviews: "http://api.rottentomatoes.com/api/public/v1.0/movies/771400429/reviews.json",
+similar: "http://api.rottentomatoes.com/api/public/v1.0/movies/771400429/similar.json"
+}
+},
+{
+id: "771410256",
+title: "Kubo and the Two Strings",
+year: 2016,
+mpaa_rating: "PG",
+runtime: 101,
+critics_consensus: "",
+release_dates: {
+theater: "2016-08-19"
+},
+ratings: {
+critics_rating: "Certified Fresh",
+critics_score: 96,
+audience_rating: "Upright",
+audience_score: 89
+},
+synopsis: "Kubo and the Two Strings is an epic action-adventure set in a fantastical Japan from acclaimed animation studio LAIKA. Clever, kindhearted Kubo ekes out a humble living, telling stories to the people of his seaside town including Hosato (George Takei), Akihiro (Cary-Hiroyuki Tagawa), and Kameyo (Academy Award nominee Brenda Vaccaro). But his relatively quiet existence is shattered when he accidentally summons a spirit from his past which storms down from the heavens to enforce an age-old vendetta. Now on the run, Kubo joins forces with Monkey (Academy Award winner Charlize Theron) and Beetle (Academy Award winner Matthew McConaughey), and sets out on a thrilling quest to save his family and solve the mystery of his fallen father, the greatest samurai warrior the world has ever known. With the help of his shamisen - a magical musical instrument - Kubo must battle gods and monsters, including the vengeful Moon King (Academy Award nominee Ralph Fiennes) and the evil twin Sisters (Academy Award nominee Rooney Mara), to unlock the secret of his legacy, reunite his family, and fulfill his heroic destiny.",
+posters: {
+thumbnail: "https://resizing.flixster.com/jOr1t45l1nBWcJquSfoCDVkp078=/54x80/v1.bTsxMjExMTgzOTtwOzE3MTA0OzIwNDg7MjE3OzMyMA",
+profile: "https://resizing.flixster.com/jOr1t45l1nBWcJquSfoCDVkp078=/54x80/v1.bTsxMjExMTgzOTtwOzE3MTA0OzIwNDg7MjE3OzMyMA",
+detailed: "https://resizing.flixster.com/jOr1t45l1nBWcJquSfoCDVkp078=/54x80/v1.bTsxMjExMTgzOTtwOzE3MTA0OzIwNDg7MjE3OzMyMA",
+original: "https://resizing.flixster.com/jOr1t45l1nBWcJquSfoCDVkp078=/54x80/v1.bTsxMjExMTgzOTtwOzE3MTA0OzIwNDg7MjE3OzMyMA"
+},
+abridged_cast: [
+{
+name: "Art Parkinson",
+id: "771443330",
+characters: [
+"Kubo"
+]
+},
+{
+name: "Charlize Theron",
+id: "162654733",
+characters: [
+"Monkey"
+]
+},
+{
+name: "Ralph Fiennes",
+id: "162653681",
+characters: [
+"Moon King"
+]
+},
+{
+name: "Rooney Mara",
+id: "770826640",
+characters: [
+"The Sisters"
+]
+},
+{
+name: "George Takei",
+id: "364615604",
+characters: [
+"Hosato"
+]
+}
+],
+links: {
+self: "http://api.rottentomatoes.com/api/public/v1.0/movies/771410256.json",
+alternate: "http://www.rottentomatoes.com/m/kubo_and_the_two_strings_2016/",
+cast: "http://api.rottentomatoes.com/api/public/v1.0/movies/771410256/cast.json",
+reviews: "http://api.rottentomatoes.com/api/public/v1.0/movies/771410256/reviews.json",
+similar: "http://api.rottentomatoes.com/api/public/v1.0/movies/771410256/similar.json"
+}
+},
+{
+id: "771365099",
+title: "Sausage Party",
+year: 2016,
+mpaa_rating: "R",
+runtime: 89,
+critics_consensus: "",
+release_dates: {
+theater: "2016-08-12"
+},
+ratings: {
+critics_rating: "Certified Fresh",
+critics_score: 82,
+audience_rating: "Upright",
+audience_score: 60
+},
+synopsis: "A misplaced sausage and his savory friends embark on an existential adventure through the aisles of a massive supermarket in this raunchy animated comedy from Seth Rogen and Evan Goldberg. As the store's annual 4th of July sale draws near, the perishable pals must return to their rightful places on the shelves before the customers come flooding in to fill their carts. Conrad Vernon (Monsters vs. Aliens) takes the helm for this Sony Pictures/AnnaPurna Pictures co-production penned by Rogen, Goldberg, Kyle Hunter, and Ariel Shaffirwhich. ~ Jason Buchanan, Rovi",
+posters: {
+thumbnail: "https://resizing.flixster.com/FSchfEfw_N9TJFE_x20axy26QMQ=/54x80/v1.bTsxMjE5Njg0NDtqOzE3MTA1OzIwNDg7MjAyNTszMDAw",
+profile: "https://resizing.flixster.com/FSchfEfw_N9TJFE_x20axy26QMQ=/54x80/v1.bTsxMjE5Njg0NDtqOzE3MTA1OzIwNDg7MjAyNTszMDAw",
+detailed: "https://resizing.flixster.com/FSchfEfw_N9TJFE_x20axy26QMQ=/54x80/v1.bTsxMjE5Njg0NDtqOzE3MTA1OzIwNDg7MjAyNTszMDAw",
+original: "https://resizing.flixster.com/FSchfEfw_N9TJFE_x20axy26QMQ=/54x80/v1.bTsxMjE5Njg0NDtqOzE3MTA1OzIwNDg7MjAyNTszMDAw"
+},
+abridged_cast: [
+{
+name: "Seth Rogen",
+id: "162653310",
+characters: [
+"Frank"
+]
+},
+{
+name: "Kristen Wiig",
+id: "770670481",
+characters: [
+"Brenda"
+]
+},
+{
+name: "Jonah Hill",
+id: "326297850",
+characters: [
+"Carl"
+]
+},
+{
+name: "Bill Hader",
+id: "742790769",
+characters: [
+"Firewater / Tequila / El Guaco"
+]
+},
+{
+name: "Michael Cera",
+id: "742790762",
+characters: [
+"Barry"
+]
+}
+],
+links: {
+self: "http://api.rottentomatoes.com/api/public/v1.0/movies/771365099.json",
+alternate: "http://www.rottentomatoes.com/m/sausage_party/",
+cast: "http://api.rottentomatoes.com/api/public/v1.0/movies/771365099/cast.json",
+reviews: "http://api.rottentomatoes.com/api/public/v1.0/movies/771365099/reviews.json",
+similar: "http://api.rottentomatoes.com/api/public/v1.0/movies/771365099/similar.json"
+}
+},
+{
+id: "771402835",
+title: "Mechanic: Resurrection",
+year: 2016,
+mpaa_rating: "R",
+runtime: 110,
+release_dates: {
+theater: "2016-08-26"
+},
+ratings: {
+critics_rating: "Rotten",
+critics_score: 22,
+audience_rating: "Spilled",
+audience_score: 51
+},
+synopsis: "Arthur Bishop (Jason Statham) returns as the Mechanic in the sequel to the 2011 action thriller. When the deceitful actions of a cunning but beautiful woman (Jessica Alba) force him to return to the life he left behind, Bishop's life is once again in danger as he has to complete an impossible list of assassinations of the most dangerous men in the world.",
+posters: {
+thumbnail: "https://resizing.flixster.com/ABRxzaw7UfYlY2UWXUrgLujvBvA=/54x80/v1.bTsxMjA2MTc3NztwOzE3MDgxOzIwNDg7NDg2OzcxOQ",
+profile: "https://resizing.flixster.com/ABRxzaw7UfYlY2UWXUrgLujvBvA=/54x80/v1.bTsxMjA2MTc3NztwOzE3MDgxOzIwNDg7NDg2OzcxOQ",
+detailed: "https://resizing.flixster.com/ABRxzaw7UfYlY2UWXUrgLujvBvA=/54x80/v1.bTsxMjA2MTc3NztwOzE3MDgxOzIwNDg7NDg2OzcxOQ",
+original: "https://resizing.flixster.com/ABRxzaw7UfYlY2UWXUrgLujvBvA=/54x80/v1.bTsxMjA2MTc3NztwOzE3MDgxOzIwNDg7NDg2OzcxOQ"
+},
+abridged_cast: [
+{
+name: "Jason Statham",
+id: "162653720",
+characters: [
+"Arthur Bishop"
+]
+},
+{
+name: "Jessica Alba",
+id: "162652782",
+characters: [
+"Gina"
+]
+},
+{
+name: "Tommy Lee Jones",
+id: "162666788",
+characters: [
+"Max Adams"
+]
+},
+{
+name: "Michelle Yeoh",
+id: "162661586",
+characters: [
+"Mae"
+]
+},
+{
+name: "Sam Hazeldine",
+id: "770777398",
+characters: [
+"Riah Crain"
+]
+}
+],
+links: {
+self: "http://api.rottentomatoes.com/api/public/v1.0/movies/771402835.json",
+alternate: "http://www.rottentomatoes.com/m/mechanic_resurrection/",
+cast: "http://api.rottentomatoes.com/api/public/v1.0/movies/771402835/cast.json",
+reviews: "http://api.rottentomatoes.com/api/public/v1.0/movies/771402835/reviews.json",
+similar: "http://api.rottentomatoes.com/api/public/v1.0/movies/771402835/similar.json"
+}
+}
+],
+links: {
+self: "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?limit=5&country=us",
+alternate: "http://www.rottentomatoes.com/movie/box-office/"
+},
+link_template: "//api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?limit={num-results}&country={country-code}"
+}
+
+
+
+
+//extracted component
+Vue.component('movie', {
+	template: '#movie-widget',
+	props: ['title', 'desc', 'rating', 'link', 'imgSrc'],
+	methods: {
+		previousMovie: function(){
+			console.log('Last Movie');
+		},
+		nextMovie: function(){
+			console.log('Next Movie');
+		}
+	}
+})
+
+var vm = new Vue({
+	el: '#widget',
+
+	data: {
+		id     :   0,
+		title  : '',
+		imgSrc : '',
+		desc   : '',
+		rating : '',
+		apiLink: "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?limit=5&country=us&apikey=6czx2pst57j3g47cvq9erte5",
+		list   : []
+	}, 
+	ready: function(){
+		console.log('instance Ready');
+		this.getList();
+	}, 
+	methods: {
+		getList: function(){
+			// this.list = mockData;
+            // this.$http.get(this.apiLink, function (data, status, request) {
+            //     // this.$set('people', data);
+            //     debugger;
+            //     this.loading = false
+            // })
+			this.list = mockData.movies
+			this.setCurMovie()
+			debugger;
+// .error(function (data, status, request) {
+//                 console.log('error');
+//             })
+        },
+
+        setCurMovie: function(){
+        	this.title = this.list[this.id].title
+        	this.imgSrc = this.list[this.id].posters.thumbnail
+        	this.desc = this.list[this.id].synopsis
+        	debugger;
+        }
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
